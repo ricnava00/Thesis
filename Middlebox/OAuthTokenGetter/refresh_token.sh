9aa4af4218f7a656ac5e7717056e24865d875746
ric00 <riccardo7.nava@mail.polimi.it>
2024-01-16 21:56:36 +0100

    Cleanup OAuth scripts

---
+++ Middlebox/OAuthTokenGetter/refresh_token.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clientId=$(grep YOUR_CLIENT_ID secrets.js | sed -r "s/.* *= *.//" | sed 's/.;//')
+clientSecret=$(grep YOUR_CLIENT_SECRET secrets.js | sed -r "s/.* *= *.//" | sed 's/.;//')
+refreshToken=$(cat refresh_token.txt | tr -d '\n')
+if [[ -z $refreshToken ]]
+then
+	echo Put the refresh token in refresh_token.txt
+	exit 1
+fi
+if [[ -z $clientId || -z $clientSecret ]]
+then
+	echo Check formatting of secrets.js
+	exit 1
+fi
+curl "https://oauth2.googleapis.com/token" -H "Content-Type: application/x-www-form-urlencoded" --data "client_id=$clientId&client_secret=$clientSecret&refresh_token=$refreshToken&grant_type=refresh_token"
